09f55beb080890f1cabc3c3ff98e8e585e1926f1
JimmyMcGil1 <92731328+JimmyMcGil1@users.noreply.github.com>
2024-01-31 17:03:23 +0700

    build: initial be

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Header, Param } from "@nestjs/common";
+import { Controller, Get, Header, Headers, HttpStatus, Param, Res, StreamableFile } from "@nestjs/common";
 import { VideoService } from "./video.service";
+import { createReadStream, statSync } from "fs";
+import { Response } from "express";
+import { join } from "path";
 
 @Controller("video")
 export class VideoController {
-    constructor(private readonly videoService: VideoService) {}
-    @Get("stream/id")
+    constructor(private readonly videoService: VideoService) { }
+    @Get("stream/:id")
     @Header("Accept-Range", "bytes")
     @Header("Content-Type", "video/mp4")
     async getStreamVideo(
@@ -12,35 +15,38 @@ export class VideoController {
         @Headers() headers,
         @Res() res: Response,
     ) {
-        const videoPath = `assets/${id}.mp4`;
+        const videoPath = `assets/${id}.mp4`
         const { size } = statSync(videoPath);
-        const videoRange = headers.range;
+        const videoRange: string = headers.range;
         if (videoRange) {
+            console.log("video range:" + videoRange);
             const parts = videoRange.replace(/bytes=/, "").split("-");
             const start = parseInt(parts[0], 10);
             const end = parts[1] ? parseInt(parts[1], 10) : size - 1;
-            const chunksize = end - start + 1;
-            const readStreamfile = createReadStream(videoPath, {
-                start,
-                end,
-                highWaterMark: 60,
-            });
+            const chunksize = (end - start) + 1;
+            const readStreamFile = createReadStream(videoPath, { start, end, highWaterMark: 20 });
             const head = {
                 "Content-Range": `bytes ${start}-${end}/${size}`,
                 "Content-Length": chunksize,
             };
-            res.writeHead(HttpStatus.PARTIAL_CONTENT, head); //206
-            readStreamfile.pipe(res);
-        } else {
-            const head = {
-                "Content-Length": size,
-            };
+            res.writeHead(HttpStatus.PARTIAL_CONTENT, head);
+            readStreamFile.pipe(res);
+
+        }
+        else {
+            const head = { "Content-Length": size, };
             res.writeHead(HttpStatus.OK, head); //200
             createReadStream(videoPath).pipe(res);
+
         }
     }
-    @Get()
-    findAll() {
-        return this.videoService.findAll();
+    @Get("/getJson")
+    async  getJson() {
+        const file =createReadStream(join(process.cwd(), "package.json"));
+        return new StreamableFile(file);
+    }
+    @Get("/")
+    async index() {
+        return "hello guy";
     }
 }
